c9b7c2941e927d1fad25768fdfed29997d4fcb13
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-03-03 23:48:03 +0300

    learn

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,15 @@ document.writeln("Hello");
 for (let i = 0; i < 10; i++) {
     document.writeln(i);
 }
-
+document.writeln("<br>");
 let user = { name: "Dima", age: 29 }
 document.writeln(user.name);
-user.name="One";
+user.name = "One";
 document.writeln(user.name);
-
+document.writeln("<br>");
+let fruit = prompt("Какой фрукт купить?", "apple"); //ввод
+let bag = {
+    [fruit]: 5, // имя свойства будет взято из переменной fruit
+  };  
+alert(bag.apple ); // 5, если fruit="apple" //вывод
+document.writeln("<br>");
