674c20d3261c93957018f68e093eb2b1a2f6aab0
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-03-03 23:35:38 +0300

    learn

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-alert('Привет');
+document.write("<h2>Заголовок h2</h2>");
+document.writeln("Hello");
+for (let i = 0; i < 10; i++) {
+    document.writeln(i);
+}
+
+let user = { name: "Dima", age: 29 }
+document.writeln(user.name);
+user.name="One";
+document.writeln(user.name);
+
