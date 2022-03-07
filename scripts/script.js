4c3b0384fb232a5facde35f1eab3d575be43f7ce
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-03-07 23:54:18 +0300

    work

--- scripts/script.js
+++ scripts/script.js
@@ -19,13 +19,6 @@ let bag = {
 alert(bag.apple); // 5, если fruit="apple" //вывод
 document.writeln("<br>");
 //////////////////////////////////////
-let user2 = {
-  name: "John",
-  surname: "Smith"
-}
-user2.name = "Pete";
-delete user2.name;
-/////////////////////////////////////
 let variable = {
   number: 21,
   word: "hello",
@@ -62,7 +55,39 @@ let codes2 = {
 for (let code in codes2) {
   document.writeln(code ); // 49, 41, 44, 1
 }
-///////////////////////////
+document.writeln("<br>");
+/////////////////////////// 
+////https://learn.javascript.ru/object#tasks////
+
+/////---1---/////
+let user2 = {
+  name: "John",
+  surname: "Smith"
+}
+document.writeln(user2.name); 
+user2.name = "Pete";
+document.writeln(user2.name); 
+delete user2.name;
+document.writeln(user2.name); 
+document.writeln("<br>");
+
+/////---2---/////
+let schedule = {};
+document.writeln(isEmpty(schedule));
+schedule["8:30"] = "get up";
+document.writeln(isEmpty(schedule));
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    // если тело цикла начнет выполняться - значит в объекте есть свойства
+    return false;
+  }
+  return true;
+}
+
+/////////////////////////////////////
+
+/////////////////////////////////////
 function Test() {
   // объявляем и инициализируем свойства
   this.x = 5;
